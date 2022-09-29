2c30ae526871d0538285b429d35817cd399cb7d4
Harun Aşık <harun.esad@gmail.com>
2022-09-29 19:13:02 +0300

    uı add

--- Assets/Scripts/Menu/PanelSwipe.cs
+++ Assets/Scripts/Menu/PanelSwipe.cs
@@ -1,16 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class PanelSwipe : MonoBehaviour
+public class PanelSwipe : MonoBehaviour, IDragHandler, IEndDragHandler
 {
-    public List<GameObject> panels;
+    private Vector3 panelLocation;
+    public float threShold = 0.2f;
+    public float smooth = 0.5f;
     void Start()
     {
-        
+        panelLocation = transform.position;
     }
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
+        float difference = eventData.pressPosition.x - eventData.position.x;
+        if (transform.position.x < 270 && transform.position.x > 85)
+        {
+            transform.position = panelLocation - new Vector3(difference, 0, 0);
+        }
+    }
 
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        float percentage = (eventData.pressPosition.x - eventData.position.x) / Screen.width;
+        if(Mathf.Abs(percentage) >= threShold && transform.position.x > 85 && transform.position.x < 270)
+        {
+            Vector3 newLocation = panelLocation;
+            if(percentage > 0 && transform.position.x > -10)
+            {
+                newLocation += new Vector3(-Screen.width, 0, 0);
+            }
+            else if (percentage < 0 && transform.position.x < 200)
+            {
+                newLocation += new Vector3(Screen.width, 0, 0);
+            }
+            StartCoroutine(SmoothMove(transform.position, newLocation, smooth));
+            panelLocation = newLocation;
+        }
+        else
+        {
+            StartCoroutine(SmoothMove(transform.position, panelLocation, smooth));
+        }
+    }
+    IEnumerator SmoothMove(Vector3 startPos, Vector3 endPos, float seconds)
+    {
+        float t = 0;
+        while (t <= 1f)
+        {
+            t += Time.deltaTime / seconds;
+            transform.position = Vector3.Lerp(startPos, endPos, Mathf.SmoothStep(0f, 1f, t));
+            yield return null;
+        }
     }
 }
---
+++ Assets/Scripts/Menu/SceneLoad.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+    GameObject playButton;
+    private void Awake()
+    {
+        playButton = GameObject.Find("PlayButton");
+        playButton.GetComponent<Button>().onClick.AddListener(PlayButton);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void PlayButton()
+    {
+        Debug.Log(playButton.name);
+        SceneManager.LoadScene(1);
+    }
+}
